a57e1d80e987fed1b9e1ddd77d2deba76d47d32b
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-04-15 13:46:31 +0530

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia";
 import { randomUUIDv7 } from "bun";
 
-const app = new Elysia()
-  .get("/", () => ({
-    id: randomUUIDv7(),
-  }))
-  .listen(3000);
+const app = new Elysia();
+
+app.get("/", () => ({
+  id: randomUUIDv7(),
+}));
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
